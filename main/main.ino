6efad5f65de305e04547c65bdd5fad27681a8ac0
Nan Yan <nan.yan@student.unsw.edu.au>
2022-05-16 12:26:12 +1000

    added analog pin setups

--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,14 @@
 #define GREEN80 7
 #define FAULT_LED 8
 
+#define BATT_VOLT A0
+#define BATT_CURR_CH A1
+#define BATT_CURR_DIS A2
+#define BATT_HYDR A3
+
+//variables
+double voltage, current;
+
 //CHARGE STATE
 enum charge_state{
   INIT, 
@@ -23,23 +31,35 @@ void setup() {
   pinMode(GREEN80, OUTPUT);
   pinMode(FAULT_LED, OUTPUT);
 
+  pinMode(BATT_VOLT, INPUT);
+  pinMode(BATT_CURR_CH, INPUT);
+  pinMode(BATT_CURR_DIS, INPUT);
+  pinMode(BATT_HYDR, INPUT);
+
 }
 
 void loop() {
+  //update sensor values
+  voltage = analogRead(BATT_VOLT);
+  current = analogRead(BATT_CURR_CH) - analogRead(BATT_CURR_DIS); // positive means batt charging, negative means batt discharging
+  
+  
+  
   //find soc
   switch(ch_state) {
+    case INIT:
+      // TODO: put in ryan code 
+      break;
     case DISCHARGE:
       soc = disch_calc_soc(voltage, a, b, c);
       break;
     case CHARGE:
-      soc = charge_soc_estim_eq(dod_prev, current, delta_t, cap);
-      break;
-    case INIT:
-      // TODO: put in ryan code 
+      soc = charge_soc_estim_eq(dod_prev, current, delta_t, cap); //define delta_t
       break;
   }
-
   //TODO: control charge state
+
+  //update battery soh
   
   //show battery soc through LEDs
   LED_indicator(soc, RED20, GREEN40, GREEN60, GREEN80);
